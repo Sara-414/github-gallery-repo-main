d8f37ec2bdaf30419a9da8eb46ad212af6b56cd8
Sara-414 <skdykstra79@gmail.com>
2023-08-09 15:33:46 -0500

    Gallery added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-//where provile information will appear
+//where profile information will appear
 const overview = document.querySelector(".overview");
+//repo list display
+const repoList = document.querySelector(".repo-list");
 const username = "Sara-414";
 
 const gitProfileInfo = async function () {
@@ -25,4 +27,20 @@ const displayUserInfo = function (userData) {
     </div>
     `;
     overview.append(div);
+    gitRepo();
+};
+
+const gitRepo = async function () {
+  const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoData = await fetchRepos.json();
+  displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
 };
